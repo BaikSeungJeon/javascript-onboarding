15dbb65ca0ad69c4ff0e4bf887b5618dffa5ac1d
BaikSeungJeon <tmdwjsofficial@naver.com>
2022-10-26 20:20:32 +0900

    docs: 기능 목록 작성

--- src/problem1.js
+++ src/problem1.js
@@ -1,26 +1,13 @@
 function problem1(pobi, crong) {
-  // 기능 1: 설계하기
-  // 시작 면과 마지막 면이 나오지 않도록 책을 펼칩니다.
-    // 페이지 === 1 || 페이지 === 400 ? false : true
-  // 둘이 연속된 수 외가 안 됩니다.
-    // [1] - [0] > 1 ? false : true
-  // 위 둘의 경우 중 하나라도 해당 되면(||) -1을 return 합니다.
-
-  // 포비의 최대수를 구합니다.
-  // 크롱의 최대수를 구합니다.
-  // 최대수는 더한 값과 곱한 값 중 Math.max를 이용해 return 해 줍니다.
-
   if(!getCheckPageNum(pobi, crong) || !getCheckPageGap(pobi, crong)) return -1;
-  
-
 }
-
-function getCheckPageNum(pobi, crong){
-  pobi.forEach(page => page === 1 || page === 400) || crong.forEach(page => page === 1 || page === 400) ? false : true
+// 페이지가 첫 번째 페이지거나 마지막 페이지인지 체크
+function getCheckPageNum(pobi, crong) {
+  return pobi.includes(1) || pobi.includes(400) || crong.includes(1) || crong.includes(400) ? false : true
 }
-
-function getCheckPageGap(pobi, crong){
-  pobi[1] - pobi[0] > 1 || crong[1] - crong[0] > 1 ? false : true;
+// 페이지가 연속된 페이지인지 아닌지 체크
+function getCheckPageGap(pobi, crong) {
+  return pobi[1] - pobi[0] > 1 || crong[1] - crong[0] > 1 ? false : true;
 }
 
 module.exports = problem1;
